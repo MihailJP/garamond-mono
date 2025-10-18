aba498f8687907c8f1c55647d30e3524989ab4c9
MihailJP <mihailjp@gmail.com>
2025-10-18 23:22:58 +0900

    Set openTypeNameVersion and postscriptIsFixedPitch

--- sources/build.sh
+++ sources/build.sh
@@ -11,6 +11,18 @@ for i in *.sfd; do
 	ttf=../fonts/ttf/${i%sfd}ttf
 	otf=../fonts/otf/${i%sfd}otf
 	sfd2ufo $i $ufo
+
+	sed -i~ -f - <<EOS $ufo/fontinfo.plist
+/<key>versionMinor<\/key>/ {
+n
+n
+s/^/<key>openTypeNameVersion<\/key>\n\
+  <string>$(grep "^Version: " $i | sed -e "s/^Version: //")<\/string>\n\
+  <key>postscriptIsFixedPitch<\/key>\n\
+  <true\/>/
+}
+EOS
+
 	fontmake -u $ufo -o ttf -f --output-path $ttf
 	fontmake -u $ufo -o otf -f --output-path $otf
 	woff2_compress $ttf && mv ${ttf%ttf}woff2 ../fonts/webfonts
