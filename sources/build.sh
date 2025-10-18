4e8d261392ead28457e344e6bc3118a8480f59d5
MihailJP <mihailjp@gmail.com>
2025-10-18 21:08:49 +0900

    Build via ufo

--- sources/build.sh
+++ sources/build.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
-for i in *.sfd; do
-	fontforge -lang=py -script <<EOS
-import fontforge
-font = fontforge.open("$i")
-font.generate("../fonts/ttf/${i%sfd}ttf")
-font.generate("../fonts/otf/${i%sfd}otf")
-font.generate("../fonts/webfonts/${i%sfd}woff2")
-font.close()
-EOS
+rm -rf ../build
+for i in ../fonts/ttf/*; do
+	rm -rf $i
 done
+mkdir ../build
+
+for i in *.sfd; do
+	ufo=../build/${i%sfd}ufo
+	ttf=../fonts/ttf/${i%sfd}ttf
+	otf=../fonts/otf/${i%sfd}otf
+	sfd2ufo $i $ufo
+	fontmake -u $ufo -o ttf --output-path $ttf
+	fontmake -u $ufo -o otf --output-path $otf
+	woff2_compress $ttf && mv ${ttf%ttf}woff2 ../fonts/webfonts
+done
+
+rm -rf ../build
