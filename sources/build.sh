bf5f4b94304b34dcb62d7c3b1da5a8ee1d893081
MihailJP <mihailjp@gmail.com>
2025-10-19 15:10:34 +0900

    build.sh now abends if it should

--- sources/build.sh
+++ sources/build.sh
@@ -10,11 +10,12 @@ for i in *.sfd; do
 	ufo=../build/${i%sfd}ufo
 	ttf=../fonts/ttf/${i%sfd}ttf
 	otf=../fonts/otf/${i%sfd}otf
-	sfd2ufo $i $ufo
-	../scripts/ufo-workaround.py $ufo $i
-	fontmake -u $ufo -o ttf -f --output-path $ttf
-	fontmake -u $ufo -o otf -f --output-path $otf
-	woff2_compress $ttf && mv ${ttf%ttf}woff2 ../fonts/webfonts
+	sfd2ufo $i $ufo || exit $?
+	../scripts/ufo-workaround.py $ufo $i || exit $?
+	fontmake -u $ufo -o ttf -f --output-path $ttf || exit $?
+	fontmake -u $ufo -o otf -f --output-path $otf || exit $?
+	woff2_compress $ttf || exit $?
+	mv ${ttf%ttf}woff2 ../fonts/webfonts
 done
 
 rm -rf ../build
