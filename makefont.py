00cbdb9c49b0daf3e700c01b19739d30daba018f
MihailJP <mihailjp@gmail.com>
2026-05-24 00:00:51 +0900

    Use MihailJP/setup-fontforge@v1

--- makefont.py
+++ makefont.py
@@ -1,28 +1,11 @@
-#!/usr/bin/env fontforge
+#!/usr/bin/env python3
 
-import fontforge, psMat
+import fontforge
 from sys import argv
-
-def decomposeNestedRefs(font):
-	while True:
-		nestedRefsFound = False
-		for glyph in font.glyphs():
-			decomposedRef = []
-			for ref in glyph.references:
-				(srcglyph, matrix, _) = ref
-				if len(font[srcglyph].references) > 0:
-					print("Glyph " + glyph.glyphname + " has a nested reference to " + srcglyph)
-					for srcref in font[srcglyph].references:
-						decomposedRef += [(srcref[0], psMat.compose(srcref[1], matrix), False)]
-					nestedRefsFound = True
-				else:
-					decomposedRef += [ref]
-			glyph.references = tuple(decomposedRef)
-		if not nestedRefsFound:
-			break
+import fontforge_refsel
 
 font = fontforge.open(argv[2])
-decomposeNestedRefs(font)
+fontforge_refsel.decomposeNestedRefs(font, True)
 font.buildOrReplaceAALTFeatures()
 
 if argv[1].endswith(".sfd"):
